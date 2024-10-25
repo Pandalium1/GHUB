fa2227ba702e2e16a396de1e94daec0a1912829b
Pandalium1 <newhackermanwow@outlook.com>
2024-10-25 09:37:30 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,25 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Pandalium1/GHUB/refs/heads/main/Games/PetGo"))()
+local FileTarget = "https://raw.githubusercontent.com/Pandalium1/GHUB/refs/heads/main/Files/"
+local Files = {
+    {PlaceId = 142823291, File = "MM2.lua"}
+}
+
+local function loadFile(placeId)
+    for _, file in ipairs(Files) do
+        if file.PlaceId == placeId then
+            local url = FileTarget .. file.File
+            local success, result = pcall(function()
+                return game:HttpGet(url)
+            end)
+            
+            if success then
+                local func, loadError = loadstring(result)
+                if func then
+                    pcall(func)
+                end
+            end
+            return
+        end
+    end
+end
+
+loadFile(game.PlaceId)
